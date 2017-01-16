509374c061740b4900ea4058bd51add9d2d772ff
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-01-16 15:33:24 +1100

    add controller methods for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+
   def index
+    @users = User.all
   end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      render json: @user
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(user_params)
+    if @user.save
+      render json: @user
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+  end
+
+  def user_params
+    params.permit(:name, :username, :password, :location)
+  end
+
 end
