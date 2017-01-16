a5fa3a116890631ad7674ab910c50a1e1a6dc375
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-01-16 15:45:16 +1100

    add controller methods for law_firms_controller

--- app/controllers/law_firms_controller.rb
+++ app/controllers/law_firms_controller.rb
@@ -1,2 +1,31 @@
 class LawFirmsController < ApplicationController
+
+  def index
+    @law_firms = Law_Firm.all
+  end
+
+  def create
+    @law_firm = Law_Firm.create(user_params)
+    if @law_firm.save
+      render json: @law_firm
+    end
+  end
+
+  def update
+    @law_firm = Law_Firm.find_by(id: params[:id])
+    @law_firm.update(user_params)
+    if @law_firm.save
+      render json: @law_firm
+    end
+  end
+
+  def destroy
+    @law_firm = Law_Firm.find_by(id: params[:id])
+    @law_firm.destroy
+  end
+
+  def law_firm_params
+    params.permit(:company_name, :location, :free_advice, :email, :phone_number)
+  end
+
 end
