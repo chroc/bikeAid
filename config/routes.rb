f0ccbf293738fddb917a8e42c787ec9c8cfed804
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-01-16 12:44:45 +1100

    generate law_firms controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :law_firms
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
